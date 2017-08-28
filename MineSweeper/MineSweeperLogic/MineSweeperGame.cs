f48b6af376c409bc34add82babc89631b1f2289d
pontuslaos <pontus.laos@lund.ntig.se>
2017-08-28 11:24:41 +0100

    Implemented method to place correct amount of mines.

--- MineSweeper/MineSweeperLogic/MineSweeperGame.cs
+++ MineSweeper/MineSweeperLogic/MineSweeperGame.cs
@@ -9,16 +9,19 @@ namespace MineSweeperLogic
 {
     public class MineSweeperGame
     {
-
+        private PositionInfo[,] _grid;
+        private IServiceBus _bus;
         #region Constructor
 
         public MineSweeperGame(int sizeX, int sizeY, int nrOfMines, IServiceBus bus)
         {
-            this.SizeX = sizeX;
-            this.SizeY = sizeY;
-            this.NumberOfMines = nrOfMines;
-            this.State = GameState.Playing;
-            this.ResetBoard();
+            SizeX = sizeX;
+            SizeY = sizeY;
+            NumberOfMines = nrOfMines;
+            State = GameState.Playing;
+            _grid = new PositionInfo[SizeX, SizeY];
+            _bus = bus;
+            ResetBoard();
         }
 
         #endregion
@@ -32,7 +35,7 @@ namespace MineSweeperLogic
 
         public PositionInfo GetCoordinate(int x, int y)
         {
-            return null;
+            return _grid[x, y];
         }
 
         public void FlagCoordinate()
@@ -47,9 +50,41 @@ namespace MineSweeperLogic
 
         public void ResetBoard()
         {
+            for (int x = 0; x < _grid.GetLength(0); x++)
+            {
+                for (int y = 0; y < _grid.GetLength(1); y++)
+                {
+                    _grid[x, y] = new PositionInfo()
+                    {
+                        X = x,
+                        Y = y,
+                        IsOpen = false
+                    };
+                }
+            }
+            PlaceNumberOfMines(NumberOfMines);
+            State = GameState.Playing;
         }
 
+        private void PlaceNumberOfMines(int nrOfMines)
+        {
+            if (nrOfMines <= 0)
+                return;
 
+            int placedMines = 0;
+
+            do
+            {
+                int x = _bus.Next(SizeX);
+                int y = _bus.Next(SizeY);
+                if (!_grid[x, y].HasMine)
+                {
+                    _grid[x, y].HasMine = true;
+                    placedMines++;
+                }
+            }
+            while (placedMines != nrOfMines);
+        }
 
         #endregion
 
